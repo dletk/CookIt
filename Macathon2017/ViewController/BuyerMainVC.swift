7c52d0fccb1a324410fb7a40279000ff32c50afc
Nam Phung <nphung@macalester.edu>
2017-11-10 22:29:08 -0600

    created segue to buyer main VC

--- Macathon2017/ViewController/BuyerMainVC.swift
+++ Macathon2017/ViewController/BuyerMainVC.swift
@@ -28,21 +28,13 @@ class BuyerMainVC: UIViewController, UITableViewDataSource, UITableViewDelegate 
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if let destination = segue.destination as? BuyerMainVC {
+//
+//        }
+//    }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
+
