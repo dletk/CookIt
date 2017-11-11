20ed99f3a0b37b17086b41c37a4c637acd6d43d6
dletk <dle@macalester.edu>
2017-11-11 00:10:58 -0600

    Finish the first view of seller

--- Macathon2017/ViewController/SellerMainVC.swift
+++ Macathon2017/ViewController/SellerMainVC.swift
@@ -8,17 +8,54 @@
 
 import UIKit
 
-class SellerMainVC: UIViewController {
-
+class SellerMainVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let item1  = Item(itemName: "Chicken", price: 12.1, location: "Saint Paul", dietaryInfo: [DietaryInfo.peanut_watch], type: "Entree", expectedNumOfServings: 2, serviceType: ServiceType.pick_up, itemImage: #imageLiteral(resourceName: "example1"))
+    let item2  = Item(itemName: "Pho", price: 9.0, location: "Saint Paul", dietaryInfo: [DietaryInfo.peanut_watch], type: "Entree", expectedNumOfServings: 6, serviceType: ServiceType.pick_up, itemImage: #imageLiteral(resourceName: "example2"))
+    let item3  = Item(itemName: "Banh xeo", price: 13.1, location: "Saint Paul", dietaryInfo: [DietaryInfo.peanut_watch], type: "Entree", expectedNumOfServings: 4, serviceType: ServiceType.pick_up, itemImage: #imageLiteral(resourceName: "example3"))
+    let item4  = Item(itemName: "Pho", price: 7.7, location: "Saint Paul", dietaryInfo: [DietaryInfo.peanut_watch], type: "Entree", expectedNumOfServings: 12, serviceType: ServiceType.pick_up, itemImage: #imageLiteral(resourceName: "example4"))
+    private var listItems: [Item] = []
+    
+    @IBOutlet weak var currentOfferItemsTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        currentOfferItemsTableView.delegate = self
+        currentOfferItemsTableView.dataSource = self
+        listItems.append(item1)
+        listItems.append(item2)
+        listItems.append(item3)
+        listItems.append(item4)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "sellItemCell", for: indexPath) as? ItemSellCell else {
+            fatalError("Cannot deque the cell")
+        }
+        let item = listItems[indexPath.row]
+        
+        cell.itemName.text = item.itemName
+        cell.itemPriceLabel.text = String(item.price)
+        cell.itemServingLabel.text = String(item.expectedNumOfServings)
+        cell.itemType.text = item.type
+        cell.itemImage.image = item.itemImage
+        
+        
+        return cell
     }
     
 
